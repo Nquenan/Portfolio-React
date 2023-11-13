ac0d814c9b5d57409e5756bc08d25eb04f526891
Natalia Quenan <112055340+Nquenan@users.noreply.github.com>
2023-11-13 13:47:57 -0300

    Front

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ import Particle from "../Particle";
 import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/Avatar-Maker.svg";
+import laptopImg from "../../Assets/19.png";
 import Toolstack from "./Toolstack";
 
 function About() {
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/Avatar-Maker.svg";
+import myImg from "../../Assets/2.png";
 import Tilt from "react-parallax-tilt";
 import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
