f6ac49cfc404a63c75f853e8be67efcd4f762f43
Natalia Quenan <112055340+Nquenan@users.noreply.github.com>
2024-02-27 10:47:03 -0300

    Update

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "left" }}>
-            Actualmente, acabo de finalizar mi formación en Silicon Misiones, donde he adquirido conocimiento y experiencia en Desarrollo Web Full-Stack.
+            Actualmente, he finalizado mi formación en Silicon Misiones, donde he adquirido conocimiento y experiencia en Desarrollo Web Full-Stack.
             <br />A lo largo de mi trayectoria, he tenido la oportunidad de viajar y mejorar mis habilidades de comunicación en inglés.
             <br />
             Mi experiencia laboral previa me ha permitido desarrollar competencias esenciales como el trabajo en equipo, adaptabilidad a los cambios, empatía y comunicación.
@@ -20,14 +20,18 @@ function AboutCard() {
           </p>
 
           <ul>
+            
             <li className="about-activity">
-              <ImPointRight /> Juegos Online
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Diseñar y Dibujar
+              <ImPointRight /> Diseño Gráfico e Ilustración Digital
             </li>
             <li className="about-activity">
               <ImPointRight /> Viajar
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Crypto DYOR
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Juegos Online
             </li>
           </ul>
 
